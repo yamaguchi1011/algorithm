5e8f4b12f7bfc7ef9000ea54da93621630f4059c
yamaguchi <yamaguchi1001@icloud.com>
2023-06-20 18:46:23 +0900

    FizzBazz2

--- fizzbazz.rb
+++ fizzbazz.rb
@@ -1,13 +1,12 @@
 arg = ARGV[0].to_i
 1.upto(arg) do |num|
   if num % 15 == 0
-    p "Fizzbazz"
+    p "FizzBazz"
   elsif num % 3 == 0
-    p "Fizz"
-  elsif num % 5 == 0
-    p "bazz"
+    p "Fizz" 
+  elsif num  % 5 == 0
+    p "Bazz" 
   else
     p num
   end
-  
 end
